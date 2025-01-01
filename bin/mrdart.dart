7a1955fbe4ea296d76e5f024677f083df8065d9b
Masud <masudurr60@gmail.com>
2025-01-01 23:29:59 +0600

    update

--- bin/mrdart.dart
+++ bin/mrdart.dart
@@ -1,61 +1,13 @@
-import 'package:args/args.dart';
+import 'dart:io';
 
-const String version = '0.0.1';
-
-ArgParser buildParser() {
-  return ArgParser()
-    ..addFlag(
-      'help',
-      abbr: 'h',
-      negatable: false,
-      help: 'Print this usage information.',
-    )
-    ..addFlag(
-      'verbose',
-      abbr: 'v',
-      negatable: false,
-      help: 'Show additional command output.',
-    )
-    ..addFlag(
-      'version',
-      negatable: false,
-      help: 'Print the tool version.',
-    );
+void triangle() {
+  print("enter the base:");
+  double? base = double.parse(stdin.readLineSync()!);
+  print("enter the height:");
+  double? height = double.parse(stdin.readLineSync()!);
+  double area = 0.5 * base * height;
+  print("area of the triangle: $area");
 }
-
-void printUsage(ArgParser argParser) {
-  print('Usage: dart mrdart.dart <flags> [arguments]');
-  print(argParser.usage);
-}
-
-void main(List<String> arguments) {
-  final ArgParser argParser = buildParser();
-  try {
-    final ArgResults results = argParser.parse(arguments);
-    bool verbose = false;
-
-    // Process the parsed arguments.
-    if (results.wasParsed('help')) {
-      printUsage(argParser);
-      return;
-    }
-    if (results.wasParsed('version')) {
-      print('mrdart version: $version');
-      return;
-    }
-    if (results.wasParsed('verbose')) {
-      verbose = true;
-    }
-
-    // Act on the arguments provided.
-    print('Positional arguments: ${results.rest}');
-    if (verbose) {
-      print('[VERBOSE] All arguments: ${results.arguments}');
-    }
-  } on FormatException catch (e) {
-    // Print usage information if an invalid argument was provided.
-    print(e.message);
-    print('');
-    printUsage(argParser);
-  }
+void main() {
+  triangle();
 }
